4f9d3e20561df400a85286fda059e1292ca09403
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2020-07-08 10:47:30 +0300

    Linked manually react native vector icons to ios/andorid native folders

--- src/components/common/SearchInput/SearchInput.tsx
+++ src/components/common/SearchInput/SearchInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TextInput, View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 interface SearchInputInputProps {
   onChange?: (value: string) => void;
@@ -9,6 +10,7 @@ interface SearchInputInputProps {
 const SearchInput = ({onChange, placeholder}: SearchInputInputProps) => {
   return (
    <View style={styles.container}>
+     <Icon name="search" size={30}  />
      <TextInput
        placeholder={placeholder}
      />

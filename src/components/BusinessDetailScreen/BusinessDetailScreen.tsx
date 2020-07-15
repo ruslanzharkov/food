2f9132248f4521d7698fcfb66fbd18bc8922c9d4
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2020-07-15 23:52:03 +0300

    Added star rating icon and title into the details screen

--- src/components/BusinessDetailScreen/BusinessDetailScreen.tsx
+++ src/components/BusinessDetailScreen/BusinessDetailScreen.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, Image, StyleSheet, Alert} from 'react-native';
+import {View, Text, Image, StyleSheet, Alert, ScrollView} from 'react-native';
 import {Props} from './index';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
 const BusinessDetailScreen = ({route, navigation, thunkGetBusinessDetailData, businessDetail}: Props) => {
   const [error, setError] = useState<string>('');
   const businessDetailError = businessDetail?.error;
+  const businessDetailInfo = businessDetail.data;
 
   useEffect(() => {
     navigation.setOptions({ title: route.params.name })
@@ -28,8 +30,19 @@ const BusinessDetailScreen = ({route, navigation, thunkGetBusinessDetailData, bu
   }
 
   return (
-    <View>
-      <Image source={{uri: businessDetail.data.image_url}} style={styles.businessImage} />
+    <View style={{position: 'relative'}}>
+      <Image source={{uri: businessDetailInfo.image_url}} style={styles.businessImage} />
+      <View style={styles.businessDetailInfoOverlay}>
+        <ScrollView style={styles.scrollView}>
+          <>
+            <View style={styles.businessDetailInfoStar}>
+              <FontAwesomeIcon name="star" size={16} style={styles.starIcon} />
+              <Text style={styles.businessDetailInfoTitle}>{businessDetailInfo.rating} Stars</Text>
+            </View>
+            <Text style={styles.businessDetailInfoName}>{businessDetailInfo.name}</Text>
+          </>
+        </ScrollView>
+      </View>
     </View>
   )
 }
@@ -39,6 +52,37 @@ export default BusinessDetailScreen;
 export const styles = StyleSheet.create({
   businessImage: {
     width: '100%',
-    height: 250
-  }
+    height: 240
+  },
+  businessDetailInfoOverlay: {
+    flex: 1,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: '#fff',
+    width: '100%',
+    height: '300%',
+    position: 'absolute',
+    top: '90%'
+  },
+  scrollView: {
+    marginHorizontal: 15,
+    marginTop: 20
+  },
+  businessDetailInfoStar: {
+    flexDirection: 'row',
+  },
+  businessDetailInfoTitle: {
+    color: '#505059'
+  },
+  businessDetailInfoName: {
+    marginTop: 5,
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  starIcon: {
+    alignSelf: 'center',
+    color: '#f7c01d',
+    paddingRight: 3,
+    borderColor: '#000'
+  },
 });
